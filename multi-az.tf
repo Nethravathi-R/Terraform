b6457fe4e8f7de538982fbbd1acd5edf10faeddf
NethravathiR <154945584+Nethravathi-R@users.noreply.github.com>
2024-02-29 20:16:00 +0530

    Update multi-az.tf

--- multi-az.tf
+++ multi-az.tf
@@ -1,10 +1,11 @@
 provider "aws" {
-    region = "ap-south-1"
+    region = "ap-northeast-3"
   
 }
 provider "aws" {
- region ="ap-northeast-2"
- alias ="instance2"
+    region = "ap-northeast-2"
+    alias = "virginia"
+  
 }
 resource "aws_instance" "ec2-mumbai" {
  ami ="ami-05ff0b3a7128cd6f8"
@@ -14,10 +15,10 @@ resource "aws_instance" "ec2-mumbai" {
  }
 }
 
-resource "aws_instance" "ec2-instance2" {
- ami ="ami-0c7217cdde317cfec"
+resource "aws_instance" "ec2-virginia" {
+ ami ="ami-0f3a440bbcff3d043"
  instance_type ="t2.micro"  
- provider = aws.instance2
+ provider = aws.virginia
  tags = {
     Name = "my-ec2"
  }
