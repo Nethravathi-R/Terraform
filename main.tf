bcb1edb5f26ef77eb7b839124f3f5e6fe369e6d3
NethravathiR <154945584+Nethravathi-R@users.noreply.github.com>
2024-03-01 16:57:06 +0530

    Update main.tf
    to excute sonarcube tomcat & static webesite just u need to update the file name

--- main.tf
+++ main.tf
@@ -1,68 +1,75 @@
 provider "aws" {
- region ="ap-northeast-3"
+    region = "us-east-1"
+  
 }
 
 data "template_file" "web-userdata" {
-  template = "${file("sonarqube.sh")}"
+        template = "${file("staticfile.sh")}"
 }
 
-resource "aws_instance" "firstInstance" {
-  ami           = "ami-05ff0b3a7128cd6f8"
-  instance_type = "t2.micro"
+resource "aws_instance" "ec2-mumbai" {
+  ami           = "ami-0c7217cdde317cfec"
+  instance_type = "t2.medium"
   vpc_security_group_ids = [aws_security_group.instance.id]
   user_data = "${data.template_file.web-userdata.rendered}"
-  key_name= "user"
   tags = {
-  Name = "my-sonar"
+    Name = "my-sonarqube"
   }
 }
 
-resource "aws_security_group" "instance"{
- name = var.aws_security_group
+resource "aws_security_group" "instance" {
 
- ingress{
-    form_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    
- }
- ingress{
-    from_port = 8082
-    to_port = 8082
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
- }
+  name = var.security_group_name
 
- ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+        ingress {
+                from_port = 80
+                to_port = 80
+                protocol = "tcp"
+                cidr_blocks = ["0.0.0.0/0"]
+        }
+       
+ingress {
+                from_port = 9000
+                to_port = 9000
+                protocol = "tcp"
+                cidr_blocks = ["0.0.0.0/0"]
+        }
+ingress {
+                from_port = 9001
+                to_port = 9001
+                protocol = "tcp"
+                cidr_blocks = ["0.0.0.0/0"]
+        }
 
-  ingress {
-     from_port = 443
-     to_port = 443
-     protocol = "tcp"
-     cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-     from_port = 0
-     to_port = 0
-     protocol = "-1"
-     cidr_blocks = ["0.0.0.0/0"]
-  }
+
+        ingress {
+                from_port = 22
+                to_port = 22
+                protocol = "tcp"
+                cidr_blocks = ["0.0.0.0/0"]
+        }
+
+         ingress {
+                from_port = 443
+                to_port = 443
+                protocol = "tcp"
+                cidr_blocks = ["0.0.0.0/0"]
+        }
+
+        egress {
+                from_port = 0
+                to_port = 0
+                protocol = "-1"
+                cidr_blocks = ["0.0.0.0/0"]
+        }
 }
-
 variable "security_group_name" {
-  description = "The  name of  the security group"
+  description = "The name of the security group"
   type        = string
-  default     = "terraform-exmaple-instance"
+  default     = "terraform-example-instance"
 }
 
-output "instance_public_ip" {
-  value = aws.instance.firstInstance.public_ip
-  description = "The public ip of Instance"
-
+output "public_ip" {
+  value       = aws_instance.ec2-mumbai.public_ip
+  description = "The public IP of the Instance"
 }
