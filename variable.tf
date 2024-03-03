397f5ca2e8b3097a8e23ab637a05ccf6677b18f0
NethravathiR <154945584+Nethravathi-R@users.noreply.github.com>
2024-03-03 12:54:12 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,21 @@
+variable "region" {
+    default = "ap-south-1"  
+}
+variable "ami-value" {
+    default = "ami-03bb6d83c60fc5f7c"  
+}
+variable "instance-type-value" {
+    default = "t2.micro"  
+}
+variable "key-value" {
+    default = "aws key"  
+}
+variable "vpc-cidr" {
+    default = "10.10.0.0/16"  
+}
+variable "subnet-cidr" {
+    default = "10.10.1.0/24"
+}
+variable "availability-zone-value" {
+    default =  "ap-south-1a"
+}
